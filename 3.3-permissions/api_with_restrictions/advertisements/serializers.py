187074bdf405ffa1796db50d63b88a49c3b25e7b
Vasilyev-Anton <vasilyev1812@gmail.com>
2022-03-31 18:24:59 +0300

    V1.4

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -22,13 +22,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
-        read_only_fields = ['user', ]
+        fields = '__all__'
 
     def create(self, validated_data):
         """Метод для создания"""
-
+        print(validated_data)
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
@@ -38,9 +36,18 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
+    def update(self, instance, validated_data):
+        validated_data["creator"] = self.context["request"].user
+        return super().update(instance, validated_data)
+
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
         # TODO: добавьте требуемую валидацию
+        adv_valid = Advertisement.objects.filter(creator=self.context["request"].user, status="OPEN").count()
+        if self.context["request"].method == 'POST' and adv_valid >= 10:
+            raise serializers.ValidationError('Превышено количество открытых объявлений')
 
+        if self.context["request"].method == 'PATCH' and adv_valid >= 10 and data.get('status') == 'OPEN':
+            raise serializers.ValidationError('Превышено количество открытых объявлений')
         return data
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrRead
 from advertisements.serializers import AdvertisementSerializer
 
 
@@ -17,5 +17,5 @@ class AdvertisementViewSet(ModelViewSet):
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+            return [IsOwnerOrRead()]
         return []
